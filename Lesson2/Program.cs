cdd2594651ccf669c4cba4e8be76024a86261d9d
vladokt <vlad78tomsk@mail.ru>
2022-04-23 19:26:46 +0700

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Pls, enter the Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello world " + name);
+            Console.Read();
+
+        }
+
+    }
+}
